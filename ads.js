a98231d665c0e52587333b2e0cfb1debf8c84359
GakuBot <gakubot.ai@gmail.com>
2018-05-25 18:33:44 +0900

    Add files via upload

--- ads.js
+++ ads.js
@@ -1,17 +1,21 @@
 //https://github.com/appfeel/admob-phonegap-build-demo
 
 function onDeviceReady() {
+  if(typeof admob === "undefined" || !admob){
+    return
+  }
+
   document.removeEventListener('deviceready', onDeviceReady, false);
 
   // Set AdMobAds options:
   admob.setOptions({
-    publisherId:          "ca-app-pub-8785449904139661/1465323866",  // Required
+    publisherId: "ca-app-pub-8785449904139661/1465323866",  // Required
     isTesting: true, // receiving test ads (do not test with real ads as your account will be banned)
     autoShowBanner: true, // auto show banners ad when loaded
+    bannerAtTop: true
   });
 
       // Start showing banners (atomatic when autoShowBanner is set to true)
-  alert("createBannerView");
   admob.createBannerView();
 }
 
