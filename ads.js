0925aec2c6ec30376bd0320ec66880cccbf9f850
GakuBot <gakubot.ai@gmail.com>
2018-05-25 17:59:11 +0900

    Add files via upload

--- ads.js
+++ ads.js
@@ -4,9 +4,12 @@ function onDeviceReady() {
   // Set AdMobAds options:
   admob.setOptions({
     publisherId:          "ca-app-pub-8785449904139661/1465323866",  // Required
+    isTesting: true, // receiving test ads (do not test with real ads as your account will be banned)
+    autoShowBanner: true, // auto show banners ad when loaded
   });
 
       // Start showing banners (atomatic when autoShowBanner is set to true)
+  alert("createBannerView");
   admob.createBannerView();
 }
 
